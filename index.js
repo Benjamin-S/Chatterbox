f8be83cea37e03bebfa4a47c304e0eede4861890
Benjamin <benjaminsymons@hotmail.com>
2017-05-18 00:13:01 +0800

    Finished tutorial

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+  });
+
   console.log('a user connected');
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function(){
