b56e1ea33c6c1b35a664a57d7a6c5b2179375727
Benjamin-S <benjaminsymons@hotmail.com>
2017-05-19 16:21:01 +0800

    Formatting improved

--- public/main.js
+++ public/main.js
@@ -1,11 +1,53 @@
 $(function () {
+
+ var $inputMessage = $('.inputMessage'); // Input message input box
+
+  // Prevents input from having injected markup
+  function cleanInput (input) {
+    return $('<div/>').text(input).text();
+  }
+
+
   var socket = io();
   $('form').submit(function(){
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
+    socket.emit('chat message', $('.inputMessage').val());
+    $('.inputMessage').val('');
     return false;
   });
+
+  function sendMessage() {
+    var message = $inputMessage.val();
+
+    message = cleanInput(message);
+
+    // if there is a non-empty message and a socket connection
+   if (message && connected) {
+     $inputMessage.val('');
+     addChatMessage({
+       username: username,
+       message: message
+     });
+     // tell server to execute 'new message' and send along one parameter
+     socket.emit('new message', message);
+   }
+  }
+
+
   socket.on('chat message', function(msg){
-    $('#messages').append($('<li>').text(msg));
+    $('#messages').append($('<div class="message-group">').append($('<div class="comment">').text(msg)));
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
