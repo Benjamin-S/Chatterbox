2bae2623601a86bb7ec64470493649f22a916ac2
Benjamin <benjaminsymons@hotmail.com>
2017-05-19 12:15:58 +0800

    Temp

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-var app = require('express')();
+var express = require('express')
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
+  var addedUser = false;
+
   socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
   });
@@ -15,8 +20,23 @@ io.on('connection', function(socket){
   socket.on('disconnect', function() {
     console.log('user disconnected');
   });
+
+  socket.on('add user', function(username) {
+    if (addedUser) return;
+
+    // username is stored in this current session only
+    socket.username = username;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
 });
 
-http.listen(3000, function(){
+http.listen(3000, function() {
   console.log('listening on *:3000');
 });
