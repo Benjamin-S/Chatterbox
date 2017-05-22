c5e18c1ef7a0e55558629dc61d6cf1337b1fe27f
Benjamin-S <benjaminsymons@hotmail.com>
2017-05-22 15:05:46 +0800

    Input validation for return key

--- public/main.js
+++ public/main.js
@@ -1,18 +1,32 @@
-$(function () {
+$(function() {
 
- var $inputMessage = $('.inputMessage'); // Input message input box
+  var $inputMessage = $('textarea'); // Input message input box
 
   // Prevents input from having injected markup
-  function cleanInput (input) {
+  function cleanInput(input) {
     return $('<div/>').text(input).text();
   }
 
-
   var socket = io();
-  $('form').submit(function(){
-    socket.emit('chat message', $('.inputMessage').val());
-    $('.inputMessage').val('');
+  $('form').submit(function() {
+    console.log('Value: ' + $('textarea').val());
+    socket.emit('chat message', $.trim($('textarea').val()));
+    $('textarea').val('');
     return false;
+  });
+
+  $("textarea").keypress(function(e) {
+    if (e.which == 13 && !e.shiftKey) {
+      if ($.trim($("textarea").val()) != "") {
+        $(this).closest("form").submit();
+        e.preventDefault();
+        return false;
+      }
+    }
+    if($(this).get(0).selectionStart == 0 && (e.keyCode == 13)) {
+      e.preventDefault();
+      return false;
+    }
   });
 
   function sendMessage() {
@@ -21,33 +35,21 @@ $(function () {
     message = cleanInput(message);
 
     // if there is a non-empty message and a socket connection
-   if (message && connected) {
-     $inputMessage.val('');
-     addChatMessage({
-       username: username,
-       message: message
-     });
-     // tell server to execute 'new message' and send along one parameter
-     socket.emit('new message', message);
-   }
+    if (message && connected) {
+      $inputMessage.val('');
+      addChatMessage({
+        username: username,
+        message: message
+      });
+      // tell server to execute 'new message' and send along one parameter
+      socket.emit('new message', message);
+    }
   }
 
 
-  socket.on('chat message', function(msg){
+  socket.on('chat message', function(msg) {
     $('.messages').append($('<div class="message-group">').append($('<div class="comment">').text(msg)));
   });
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 });
